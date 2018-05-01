3c428341840ce2d1a0d3e66641b4fc64de1fbf08
returnofthelambda <wblillyman3@gmail.com>
2018-05-01 09:25:54 -0400

    cleaned up game_scrape and player_scrape modules, tested and working. \n\n up next: flatten out json strings in game_scrape

--- play_scrape.py
+++ play_scrape.py
@@ -6,8 +6,8 @@ def game_scrape(game_id, season):
     from pandas.io.json import json_normalize
     import urllib.request, json
 
-    game_url = 'http://statsapi.web.nhl.com/api/v1/game/'+season+gameId+'/feed/live'
-    with urllib.request.urlopen(gameUrl) as url:
+    game_url = 'http://statsapi.web.nhl.com/api/v1/game/'+season+game_id+'/feed/live'
+    with urllib.request.urlopen(game_url) as url:
         data = json.loads(url.read().decode())
 
     all_plays=data['liveData']['plays']['allPlays']
@@ -18,22 +18,26 @@ def game_scrape(game_id, season):
     skaters, goalies=players_scrape(data['liveData']['boxscore']['teams'])
     skaters['game_id']=game_id
     goalies['game_id']=game_id
-    return playData, skaters, goalies
+    return play_data, skaters, goalies
 
-def players_scrape(players):
+def players_scrape(players_json):
     '''take the data['liveData']['boxscore']['teams'] json string and parse game data for each
     player.'''
 
+    import pandas as pd
+    
+    tmp_dict={}
+    tmp_dict['goalieStats']={}
+    tmp_dict['skaterStats']={}
     for team in ['home','away']:
+        players=list(players_json[team]['players'].keys())
+        tmp_stats=players_json[team]['players']
+        for p in players:
+            for playerType in ['skaterStats','goalieStats']:
+                if playerType in tmp_stats[p]['stats']:
+                    tmp_dict[playerType].update({p[2:] : tmp_stats[p]['stats'][playerType]})
+    
+    skaters_df=pd.DataFrame(tmp_dict['skaterStats']).T
+    goalies_df=pd.DataFrame(tmp_dict['goalieStats']).T
 
-        skaters=players[team]['skaters']
-        goalies=players[team]['goalies']
-        goalies_stats=[players[team]['players']['ID'+str(g)]['stats']['goalieStats'] for g in goalies]
-        skaters_stats=[players[team]['players']['ID'+str(g)]['stats']['skaterStats'] for g in skaters]
-        for i in range(len(skaters)):
-            skaters_stats[i]['player_id']=s 
-        for i in range(len(goalies):    
-            goalies_stats[i]['player_id']=g
-        
-
-    return skaters_stats, goalies_stats
+    return skaters_df, goalies_df
